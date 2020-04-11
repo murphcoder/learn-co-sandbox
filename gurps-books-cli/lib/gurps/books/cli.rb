936c57c011b5553f0af1be3f138c12856dbe9443
Sean Murphy <filmmakersean@fastmail.net>
2020-04-11 19:45:26 +0000

    first commit

---
+++ gurps-books-cli/lib/gurps/books/cli.rb
@@ -0,0 +1,10 @@
+require "gurps/books/cli/version"
+
+module Gurps
+  module Books
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ gurps-books-cli/lib/gurps/books/cli/version.rb
@@ -0,0 +1,7 @@
+module Gurps
+  module Books
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
